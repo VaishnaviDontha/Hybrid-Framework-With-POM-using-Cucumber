7a127db7b5353980150f4e1bd59634ff4f3c3fbc
Vaishnavi Dontha <77274387+VaishnaviDontha@users.noreply.github.com>
2022-12-20 18:13:11 -0500

    Initial setup for the framework

--- cucumber/src/main/java/com/factories/DriverFactory.java
+++ cucumber/src/main/java/com/factories/DriverFactory.java
@@ -1,55 +1,57 @@
 package com.factories;
 
+import java.util.Objects;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverFactory {
 
     public WebDriver driver;
 
-    static ThreadLocal<WebDriver> tldriver = new ThreadLocal<>();
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
-    /**
-     * This method used to initialise thee driver based on the browser input given
-     * @param browser
-     * @return getDriver()
-     */
-    public WebDriver setDriver(String browser) {
+	/**
+	 * This method is used to initialize the thradlocal driver on the basis of given
+	 * browser
+	 * 
+	 * @param browser
+	 * @return this will return tldriver.
+	 */
+	public WebDriver init_driver(String browser) {
 
-        if (browser.equals("chrome")) {
-            tldriver.set(new ChromeDriver());
-        } else if (browser.equals("firefox")) {
-            tldriver.set(new FirefoxDriver());
-        } else if (browser.equals("safari")) {
-            tldriver.set(new SafariDriver());
-        } else {
-            System.out.println("Please pass correcct value of the browser");
-        }
+		System.out.println("browser value is: " + browser);
 
-        getDriver().manage().deleteAllCookies();
-        getDriver().manage().window().maximize();
+		if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			tlDriver.set(new ChromeDriver());
+		} else if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		} else if (browser.equals("safari")) {
+			tlDriver.set(new SafariDriver());
+		} else {
+			System.out.println("Please pass the correct browser value: " + browser);
+		}
 
-        return getDriver();
-    }
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		return getDriver();
 
-    /**
-     * Adding "synchronized" Keyword to ensure the threads are in-sync when running in Parallel.
-     * This meethod is used to get Driver using Threadlocal
-     * @return 
-     */
-    public static synchronized WebDriver getDriver() {
+	}
 
-        return tldriver.get();
-
-    }
-
-    public void unload() {
-
-        tldriver.remove();
-        
-    }
+	/**
+	 * this is used to get the driver with ThreadLocal
+	 * 
+	 * @return
+	 */
+	public static synchronized WebDriver getDriver() {
+		return tlDriver.get();
+	}
 
 
 
